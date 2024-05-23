3df267d6bee9362bb28de9090ba43c59dd024ebc
victory benson <victorybenson98@gmail.com>
2024-05-23 19:14:59 +0100

    update

--- client/src/component/HowItWorks.jsx
+++ client/src/component/HowItWorks.jsx
@@ -27,26 +27,26 @@ useEffect(() => {
 },[])
 
   return (
-    <div data-aos="fade-" className=' flex flex-col items-center justify-center text-center py-5 sm:p-10'>
-        <p className='font-semibold text-xl md:text-3xl font-poppins py-5 md:py-10'>Why choose us?</p>
-        <div className='w-full h-full overflow-x-scroll whitespace-nowrap scrollbar-hide scroll-smooth py-5 transition-all mx-2'>
-          {
-            services.map((data, index) => {
-              return (
-                <div key={index} className='bg-gray/10 w-72 h-40 mx-3 inline-block cursor-pointer p-2 text-center overflow-hidden'>
-                  <div className='flex flex-col justify-center items-center h-full'>
-                    <p className='text-2xl md:text-5xl text-gray'><data.icon /></p>
-                    <p className='capitalize p-2 font-semibold text-lg md:text-xl'>{data.title}</p>
-                    <p className='text-base'>{data.details}</p>
+    <section data-aos="fade-up">
+      <div className=' flex flex-col items-center justify-center h-full text-center py-5 sm:p-10'>
+          <p className='font-semibold text-xl md:text-3xl font-poppins py-5 md:py-10'>Why choose us?</p>
+          <div className='grid gird-cols1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center py-4 w-[90%] md:w-[100%]'>
+            {
+              services.map((data, index) => {
+                return (
+                  <div key={index} className='bg-gray/5 w-full h-48 md:h-52 flex flex-col items-center justify-center cursor-pointer p-2 text-center '>
+                      <p className='text-4xl md:text-5xl text-gray'><data.icon /></p>
+                      <p className='capitalize p-2 font-semibold text-lg md:text-xl'>{data.title}</p>
+                      <p className='text-base'>{data.details}</p>
                   </div>
-                </div>
-              )
-            })
-          }
-        </div>
-        <div data-aos="flip-right" onClick={() => {navigate("layout/shop");scrollToTop() }}>
-          <Btn text="Shop now" />
-        </div>
-    </div>
+                )
+              })
+            }
+          </div>
+          <div data-aos="flip-right" onClick={() => {navigate("layout/shop");scrollToTop() }} className='py-4'>
+            <Btn text="Shop now" />
+          </div>
+      </div>
+    </section>
   )
 }
