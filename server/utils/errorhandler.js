3a5d74a04f45fe2a4a38c7298e84ae438aac235a
victory benson <victorybenson98@gmail.com>
2024-06-06 16:05:28 +0100

    minor fix

--- server/utils/errorhandler.js
+++ server/utils/errorhandler.js
@@ -1,19 +1,11 @@
-// export const errorHandler = (statusCode, message) =>{
-//   const error = new Error()
-//   error.statusCode = statusCode;
-//   error.message = message;
-//   return error
-// }
 
 export const errorHandler = (err, req, res, next) => {
-  console.log("Server Error!");
   const errStatus = err.statusCode || 500;
   const errMsg = err.message || "Something went wrong";
   res.status(errStatus).json({
     success: false,
     status: errStatus,
     message: errMsg,
-    stack: err.stack
-    // stack: process.env.NODE_ENV === "development" ? err.stack : {},
+    stack: process.env.NODE_ENV === "development" ? err.stack : {},
   });
 };
